c9181a6d7030b5d7fe2572e1d042a2015b5eda5f
xuzixx <wxyahoo@gmail.com>
2015-02-16 17:58:35 +0800

    shell exec

---
+++ shell/exec/2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# reassign-stdout.sh
+
+# Similarly, an exec >filename command redirects stdout to a designated file.
+# This sends all command output that would normally go to stdout to that file.
+
+LOGFILE=logfile.txt
+
+exec 6>&1           # Link file descriptor #6 with stdout.
+                    # Saves stdout.
+
+exec > $LOGFILE     # stdout replaced with file "logfile.txt".
+
+# ----------------------------------------------------------- #
+# All output from commands in this block sent to file $LOGFILE.
+
+echo -n "Logfile: "
+date
+echo "-------------------------------------"
+echo
+
+echo "Output of \"ls -al\" command"
+echo
+ls -al
+echo; echo
+echo "Output of \"df\" command"
+echo
+df
+
+# ----------------------------------------------------------- #
+
+exec 1>&6 6>&-      # Restore stdout and close file descriptor #6.
+
+echo
+echo "== stdout now restored to default == "
+echo
+ls -al
+echo
+
+exit 0
+
