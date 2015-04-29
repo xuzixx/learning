663da47d17d22c75ad3514e4c5b1c11145cdf1de
xuzixx <wxyahoo@gmail.com>
2015-04-29 23:00:21 +0800

    学习metaclass

---
+++ py/metaclass/dynamic_class_attr.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+#coding:utf-8 
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+""" 你可以自由的、动态的修改/增加/删除 类的或者实例中的方法或者属性 """
+def ma(cls):
+    print "method a"
+
+def mb(cls):
+    print "method b"
+
+method_dict = {
+    'ma' : ma,
+    'mb' : mb,
+}
+
+class DynamicMethod(type):
+    def __new__(cls, name, bases, dct):
+        """
+        cls: 将要创建的类，类似与self，但是self指向的是instance，而这里cls指向的是class
+        name: 类的名字，也就是我们通常用类名.__name__获取的。
+        bases: 基类
+        attrs: 属性的dict。dict的内容可以是变量(类属性），也可以是函数（类方法）。
+        """
+        if name[:3] == 'Abc':
+            dct.update(method_dict)
+        return type.__new__(cls, name, bases, dct)
+
+    def __init__(cls, name, bases, dct):
+        super(DynamicMethod, cls).__init__(name, bases, dct)
+
+class AbcTest(object):
+    __metaclass__ = DynamicMethod
+    def mc(self, x):
+        print x * 3
+
+class NotAbc(object):
+    __metaclass__ = DynamicMethod
+    def md(self, x):
+        print x * 3
+
+def main():
+    """
+    通过DynamicMethod这个metaclass的原型，我们可以在那些指定了__metaclass__属性位DynamicMethod的类里面，
+    根据类名字，如果是以'Abc'开头的就给它加上ma和mb的方法(这里的条件只是一种简单的例子假设了，实际应用上
+    可能更复杂）,如果不是'Abc'开头的类就不加. 这样就可以打到动态添加方法的效果了。其实，你也可以将需要动态
+    添加或者修改的方法改到__new__里面，因为python是支持在方法里面再定义方法的. 通过这个例子，其实可以看到
+    只要我们能操作__new__方法里面的其中一个参数attrs，就可以动态添加东西了。
+    """
+    global method_dict
+    print method_dict
+    a = AbcTest()
+    a.mc(3)
+    a.ma()
+    print "[instance a] %s" % dir(a)
+    """ 全局修改了method_dict 也不会使instance c方法改变 """
+    method_dict = {'me':ma}
+    print "[instance a] %s" % dir(a)
+    c = AbcTest()
+    print "[instance c] %s" % dir(c)
+    b = NotAbc()
+    print "[instance b] %s" % dir(b)
+
+if __name__ == '__main__':
+    main()
+
---
+++ py/metaclass/same_as_decorator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#coding:utf-8 
+
+from types import FunctionType 
+
+def login_required(func):
+    print 'login check logic here'
+    return func
+
+class LoginDecorator(type): 
+    def __new__(cls, name, bases, dct):
+        for name, value in dct.iteritems():
+            if name not in ('__metaclass__', '__init__', '__module__') and\
+                type(value) == FunctionType:
+                value = login_required(value)
+                
+            dct[name] = value
+        return type.__new__(cls, name, bases, dct)
+
+class Operation(object):  
+    __metaclass__ = LoginDecorator  
+    def delete(self, x): 
+        print 'deleted %s' % str(x)  
+
+def main():
+    """
+    给 Operation 的实例的所有方法 都添加过滤
+    可以不用在delete函数上面写@login_required, 也能达到decorator的效果了。不过可读性就差点了。
+    """
+    op = Operation()  
+    op.delete('test')
+
+if __name__ == '__main__': 
+    main()
