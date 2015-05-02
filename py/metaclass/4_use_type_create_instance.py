f84077bbd2d9e9c8384fcaa84ad74a276d264f10
xuzixx <wxyahoo@gmail.com>
2015-05-02 10:30:33 +0800

    patch and type ex

--- py/metaclass/4_use_type_create_instance.py
+++ py/metaclass/4_use_type_create_instance.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 #coding:utf-8 
 
+def add_method(self):
+    print "%s add method" % self.__class__
+
 class MyBase(object):
     def __init__(self):
         print "Base init"
@@ -11,13 +14,22 @@ class MyClass(MyBase):
 
 if __name__ == '__main__':
     """ 利用type 动态生成 """
-    TypeCreateClass = type('MyClass',(MyBase,),{})
-    print TypeCreateClass
-    print dir(TypeCreateClass)
-    print dir(MyClass)
+    TypeCreateClassA = type('MyClass',(MyBase,),{})
+    TypeCreateClassB = type('MyClass',(MyClass,),{})
+    TypeCreateClassC = type('MyClass',(MyClass,),{"add_method" : add_method})
+    print "Myclass %s" % dir(MyClass)
+    print "TypeCreateClassA %s" % dir(TypeCreateClassA)
+    print "TypeCreateClassB %s" % dir(TypeCreateClassB)
+    print "TypeCreateClassC %s" % dir(TypeCreateClassC)
+
     print "--- type new instance"
-    instance = TypeCreateClass()
-    print instance
+    a = TypeCreateClassA()
+    b = TypeCreateClassB(1)
+    c = TypeCreateClassC(2)
+    print "TypeCreateClassA %s" % a
+    print "TypeCreateClassB %s" % b
+    print "TypeCreateClassC %s" % c
+
     print "--- init new instance"
     mi = MyClass(4)
     print mi
