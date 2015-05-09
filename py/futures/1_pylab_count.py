1f81a1f63a5c90ee74f8029243ffaea759a1bc9a
xuzixx <wxyahoo@gmail.com>
2015-05-10 00:01:11 +0800

    update py/futures

---
+++ py/futures/1_pylab_count.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#-*- Encoding: utf-8 -*-
+
+from pylab import *
+import random
+import datetime
+
+dates = []
+counts = []
+
+# 生成上个月25 日 到本月 24日 的区间日期
+today = datetime.date.today()
+endday = today.replace(day = 24)
+
+lastm = today.replace(day = 1) - datetime.timedelta(days = 1)
+startday = lastm.replace(day = 25)
+
+#dates = [startday + datetime.timedelta(days = i) for i in xrange((endday - startday).days + 1)]
+# -------------------------------------------
+
+for i in xrange((endday - startday).days + 1):
+    dates.append(startday + datetime.timedelta(days = i))
+    counts.append(int(random.random() * 100))
+
+plot(dates, counts)
+ylim(0, max(counts))
+title("test pylab")
+#show()
+savefig("1_pylab.png")
+
+
+
--- py/futures/README.md
+++ py/futures/README.md
@@ -1,2 +1,8 @@
+文章学习
+packages:
+    glob
+    gzip
+
+
 refer:
 http://www.dalkescientific.com/writings/diary/archive/2012/01/19/concurrent.futures.html
