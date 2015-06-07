a34dd7b4f299403da011154e78d47dccd1f8a78b
xuzixx <wxyahoo@gmail.com>
2015-06-07 13:45:06 +0800

    MEDIA set update

--- py/django/mysite/mysite/urls.py
+++ py/django/mysite/mysite/urls.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+#-*- Encoding: utf-8 -*-
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,7 +15,9 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -23,3 +27,5 @@ urlpatterns = [
     # test add app
     url(r'^questions/', include('questions.urls', namespace = 'questions')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- py/django/mysite/questions/migrations/0001_initial.py
+++ py/django/mysite/questions/migrations/0001_initial.py
@@ -11,11 +11,86 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Paper',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True)),
+                ('create_time', models.DateTimeField(auto_now_add=True, verbose_name=b'\xe5\x88\x9b\xe5\xbb\xba\xe6\x97\xb6\xe9\x97\xb4')),
+                ('update_time', models.DateTimeField(auto_now=True, verbose_name=b'\xe6\x9b\xb4\xe6\x96\xb0\xe6\x97\xb6\xe9\x97\xb4')),
+                ('title', models.CharField(default=b'TEST (2015-06-07)', max_length=250, verbose_name=b'\xe8\xaf\x95\xe5\x8d\xb7\xe6\xa0\x87\xe9\xa2\x98')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='PaperQuestionRelation',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True)),
+                ('create_time', models.DateTimeField(auto_now_add=True, verbose_name=b'\xe5\x88\x9b\xe5\xbb\xba\xe6\x97\xb6\xe9\x97\xb4')),
+                ('update_time', models.DateTimeField(auto_now=True, verbose_name=b'\xe6\x9b\xb4\xe6\x96\xb0\xe6\x97\xb6\xe9\x97\xb4')),
+                ('seq_num', models.IntegerField(verbose_name=b'\xe9\xa2\x98\xe7\x9b\xae\xe5\xba\x8f\xe5\x8f\xb7')),
+                ('user_answer', models.CharField(max_length=500, verbose_name=b'\xe7\xad\x94\xe6\xa1\x88')),
+                ('score', models.IntegerField(verbose_name=b'\xe5\xbe\x97\xe5\x88\x86')),
+                ('paper', models.ForeignKey(related_name='question_relations', to='questions.Paper')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='PaperUser',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True)),
+                ('create_time', models.DateTimeField(auto_now_add=True, verbose_name=b'\xe5\x88\x9b\xe5\xbb\xba\xe6\x97\xb6\xe9\x97\xb4')),
+                ('update_time', models.DateTimeField(auto_now=True, verbose_name=b'\xe6\x9b\xb4\xe6\x96\xb0\xe6\x97\xb6\xe9\x97\xb4')),
+                ('name', models.CharField(max_length=50, verbose_name=b'\xe7\x94\xa8\xe6\x88\xb7\xe5\x90\x8d')),
+                ('email', models.EmailField(max_length=254, verbose_name=b'\xe9\x82\xae\xe7\xae\xb1')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
             name='Question',
             fields=[
                 ('id', models.AutoField(serialize=False, primary_key=True)),
-                ('type', models.CharField(max_length=5, choices=[(b'SC', b'\xe5\x8d\x95\xe9\x80\x89\xe9\xa2\x98'), (b'MC', b'\xe5\xa4\x9a\xe9\x80\x89\xe9\xa2\x98'), (b'SA', b'\xe7\xae\x80\xe7\xad\x94\xe9\xa2\x98')])),
-                ('content', models.CharField(max_length=250)),
+                ('create_time', models.DateTimeField(auto_now_add=True, verbose_name=b'\xe5\x88\x9b\xe5\xbb\xba\xe6\x97\xb6\xe9\x97\xb4')),
+                ('update_time', models.DateTimeField(auto_now=True, verbose_name=b'\xe6\x9b\xb4\xe6\x96\xb0\xe6\x97\xb6\xe9\x97\xb4')),
+                ('type', models.CharField(max_length=5, verbose_name=b'\xe9\xa2\x98\xe7\x9b\xae\xe7\xb1\xbb\xe5\x9e\x8b', choices=[(b'SC', b'\xe5\x8d\x95\xe9\x80\x89\xe9\xa2\x98'), (b'MC', b'\xe5\xa4\x9a\xe9\x80\x89\xe9\xa2\x98'), (b'SA', b'\xe7\xae\x80\xe7\xad\x94\xe9\xa2\x98')])),
+                ('title', models.CharField(max_length=250, verbose_name=b'\xe9\xa2\x98\xe7\x9b\xae\xe6\xa0\x87\xe9\xa2\x98')),
+                ('problem', models.TextField(default=b'', verbose_name=b'\xe9\xa2\x98\xe7\x9b\xae')),
+                ('answer', models.CharField(default=b'', max_length=250, verbose_name=b'\xe7\xad\x94\xe6\xa1\x88')),
             ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='QuestionPic',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True)),
+                ('create_time', models.DateTimeField(auto_now_add=True, verbose_name=b'\xe5\x88\x9b\xe5\xbb\xba\xe6\x97\xb6\xe9\x97\xb4')),
+                ('update_time', models.DateTimeField(auto_now=True, verbose_name=b'\xe6\x9b\xb4\xe6\x96\xb0\xe6\x97\xb6\xe9\x97\xb4')),
+                ('pic', models.ImageField(upload_to=b'questions/QuestionPic/%Y/%m/%d', blank=True)),
+                ('question', models.ForeignKey(related_name='question_pics', to='questions.Question')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.AddField(
+            model_name='paperquestionrelation',
+            name='question',
+            field=models.ForeignKey(related_name='paper_relations', to='questions.Question'),
+        ),
+        migrations.AddField(
+            model_name='paper',
+            name='questions',
+            field=models.ManyToManyField(related_name='papers', through='questions.PaperQuestionRelation', to='questions.Question'),
+        ),
+        migrations.AddField(
+            model_name='paper',
+            name='user',
+            field=models.ForeignKey(related_query_name=b'paper', related_name='papers', to='questions.PaperUser'),
         ),
     ]
